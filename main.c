3b1821fd5622982d8fc4ee19569284aeeb3e52c2
PeterSalnikov <petersalnikov00@gmail.com>
2024-01-23 12:41:31 -0800

    LED practice

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
+#include<stdio.h>
+
+#define DA_TRIGGER_FILE_NAME_HERE "/sys/class/leds/beaglebone:green:usr0/trigger"
+#define DA_BRIGHTNESS_FILE_NAME_HERE "/sys/class/leds/beaglebone:green:usr0/brightness"
+
 int main()
 {
-    printf("Hello embedded world, from Peter!\n");
+    
+
     return 0;
 }
