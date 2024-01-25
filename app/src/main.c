4400451d38a25f3fd47b729fa05d46807d61fe41
PeterSalnikov <petersalnikov00@gmail.com>
2024-01-24 21:11:09 -0800

    refactored LED + prepared joystick

--- app/src/main.c
+++ app/src/main.c
@@ -22,60 +22,45 @@ static void sleepForMs(long long delayInMs)
     nanosleep(&reqDelay, (struct timespec *) NULL);
 }
 
-void joystick_getCurrentDirection()
-{
-    // just a 5-way conditional that sees whose value is 0, if any?
-    // run this in an event loop and react to the value it changes to?
-    FILE *up = fopen("/sys/class/gpio/gpio26/value","r");
-    if(up == NULL) {
-        printf("ERROR: joystick_getCurrentDirection: \"UP\" binding not found\n");
-        exit(1);
-    }
-    printf("%c\n",fgetc(up));
-    fclose(up);
-    // return NONE;
-}
+
 
 int main()
 {
     // Initialize all modules; HAL modules first
     button_init();
-    // printf("here");
-    struct LED *leds = led_init();
 
 // testing out some functions
     for(int i = 0; i < NUM_LEDS; i++) {
 
         // int brightnessChanged = fprintf(leds[i].brightness,"%d",i % 2);
-        led_setTrigger(leds[i].trigger,"none");
+        led_setTrigger(i,"none");
 
-        led_setBrightness(leds[i].brightness,"0");
+        led_setBrightness(i,"0");
     }
 
-    // led_cleanup(leds);
     // Main program logic:-
     // // Print a "get ready" message and turn on the middle two LEDs on BBG.
     printf("Get Ready..!");
     for(int i = 1; i < 3; i++) {
-        led_setBrightness(leds[i].brightness, "0");
+        led_setBrightness(i, "0");
     }
 
     // // if the user is pressing the joystick, tell them "Please let go of joystick" and wait until the joystick is not pressing.
 
     while(1) {
-        leds = led_init();
+
         for(int i = 1; i < 3; i++) {
-            led_setBrightness(leds[i].brightness, "0");
+            led_setBrightness(i, "0");
         }
-        led_cleanup(leds);
+
         sleepForMs(100);
         
         joystick_getCurrentDirection();
-        leds = led_init();
+
         for(int i = 1; i < 3; i++) {
-            led_setBrightness(leds[i].brightness, "1");
+            led_setBrightness(i, "1");
         }
-        led_cleanup(leds);
+
         sleepForMs(100);
 
     }
--- hal/src/joystick.c
+++ hal/src/joystick.c
@@ -1,2 +1,23 @@
 #include "hal/joystick.h"
 
+static bool is_initialized = false;
+
+void joystick_init()
+{
+    // here, would check if all necessary GPIO pins are exported. In this assignment, not to worry
+    is_initialized = true;
+}
+
+void joystick_getCurrentDirection()
+{
+    // just a 5-way conditional that sees whose value is 0, if any?
+    // run this in an event loop and react to the value it changes to?
+    FILE *up = fopen("/sys/class/gpio/gpio26/value","r");
+    if(up == NULL) {
+        printf("ERROR: joystick_getCurrentDirection: \"UP\" binding not found\n");
+        exit(1);
+    }
+    printf("%c\n",fgetc(up));
+    fclose(up);
+    // return NONE;
+}
