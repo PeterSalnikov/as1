d0e8f94a62284f331286a1484e02ad706f6165cd
PeterSalnikov <petersalnikov00@gmail.com>
2024-01-29 12:50:13 -0800

    added more comments

--- hal/include/hal/joystick.h
+++ hal/include/hal/joystick.h
@@ -23,12 +23,19 @@ enum Direction {
     NONE = -1
 };
 
-
-FILE *joystick_openGPIOFile(int dirNum, char *toOpen, char *perm);
-void joystick_closeGPIOFile(FILE *GPIOFile);
-void joystick_init(void);
+// Helpers to work within joystick.c
 int i2j(int joystickInd);
 int j2i(enum Direction joystickDirection);
+FILE *joystick_openGPIOFile(int dirNum, char *toOpen, char *perm);
+void joystick_closeGPIOFile(FILE *GPIOFile);
+
+// This is here because only joystick uses it at the moment, but with a different project should probably move it to utils
+static void runCommand(char *command);
+
+// Joystick initialization
+void joystick_init(void);
+
+// High-level user functions
 bool joystick_isPressed(void);
 enum Direction joystick_getCurrentDirection(void);
 
--- hal/include/hal/led.h
+++ hal/include/hal/led.h
@@ -11,8 +11,14 @@
 #define BBG_LED_DIR "/sys/class/leds/beaglebone:green:usr"
 #define LED_BUF 50
 
+// Helpers to work within led.c
+FILE *led_openFile(int ledNum, char *fileType);
+void led_close_file(FILE *ledFile);
+
+// Higher-level functions for user
 void led_setTrigger(int ledNum, char *state);
 void led_setBrightness(int ledNum, char *level);
+
 void led_turnOffAll(void);
 void led_turnOnAll(void);
 void led_flashAll(int freqInHz, float numSeconds);
